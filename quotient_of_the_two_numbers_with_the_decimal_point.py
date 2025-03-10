fd7d5215b83279038b85897def64837a7bdb0001
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-09 19:36:56 -0800

    calculate_the_product

--- quotient_of_the_two_numbers_with_the_decimal_point.py
+++ quotient_of_the_two_numbers_with_the_decimal_point.py
@@ -4,3 +4,6 @@ num2 = float(input("Enter the second number: "))
 #check if the second number is not zero to avoid division by zero
 if num2 == 0:
     print("ERROR: Division by zero is not allowed.")
+else:
+    #calculate the quotient of the two numbers
+    
