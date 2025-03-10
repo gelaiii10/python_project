b5602e1a9ddcf28e5a114bb8cf1f4c0e2f5be1e7
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-09 20:59:41 -0800

    ask_user_to_input_number

--- how_many_are_odd_numbers.py
+++ how_many_are_odd_numbers.py
@@ -3,7 +3,7 @@ odd_count = 0
 
 # for loop to get 10 numbers from the user
 for i in range(10):
-    
 #ask the user to input a number and convert it to int
+    num = int(input(f"Enter Number {i + 1}: "))
 #check if the number is odd
 #print the total count of odd numbers
