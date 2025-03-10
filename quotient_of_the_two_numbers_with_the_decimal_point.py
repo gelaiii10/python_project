db93036f9cb191cf6f26cc4a39086b71db3a2f4c
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-09 19:28:37 -0800

    second_number_must_not_zero

--- quotient_of_the_two_numbers_with_the_decimal_point.py
+++ quotient_of_the_two_numbers_with_the_decimal_point.py
@@ -1,3 +1,6 @@
 #ask user to input two numbers and convert it to float
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+#check if the second number is not zero to avoid division by zero
+if num2 == 0:
+    print("ERROR: Division by zero is not allowed.")
