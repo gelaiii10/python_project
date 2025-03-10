252576767828f73fc19d61bbfe2412cb03a20f18
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-09 20:10:25 -0800

    input_two_numbers

--- print_result_when_the_first_number_is_raised_to_the_second_number.py
+++ print_result_when_the_first_number_is_raised_to_the_second_number.py
@@ -1,3 +1,6 @@
 #ask user to input two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 #calculate the result of num1 raised to num2
 #print the result
