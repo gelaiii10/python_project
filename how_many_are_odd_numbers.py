8763ec97d01531efaf10953b4a92e314f086f9c6
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-09 20:56:32 -0800

    added_for_loop

--- how_many_are_odd_numbers.py
+++ how_many_are_odd_numbers.py
@@ -2,6 +2,8 @@
 odd_count = 0
 
 # for loop to get 10 numbers from the user
+for i in range(10):
+    
 #ask the user to input a number and convert it to int
 #check if the number is odd
 #print the total count of odd numbers
