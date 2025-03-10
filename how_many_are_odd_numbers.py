e243e6b78c3f18a83b5b0b411bc18e67609d0712
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-09 20:53:58 -0800

    initialize_the_count_of_odd_numbers

--- how_many_are_odd_numbers.py
+++ how_many_are_odd_numbers.py
@@ -1,4 +1,6 @@
 #initialize the count of odd numbers to zero
+odd_count = 0
+
 # for loop to get 10 numbers from the user
 #ask the user to input a number and convert it to int
 #check if the number is odd
