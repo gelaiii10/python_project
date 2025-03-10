427257a512d8477cabdf192ce3b2a5988b7fad4e
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-09 19:14:26 -0800

    ask_user_to_input_two_numbers

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -1,3 +1,5 @@
 #ask user to input two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 #calculate the product of two numbers
 #print the product
