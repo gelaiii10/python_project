9e164896f289fafbd043041be8b0e66a1503dc21
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-09 20:12:52 -0800

    result_num1_raised_to_power_of_num2

--- print_result_when_the_first_number_is_raised_to_the_second_number.py
+++ print_result_when_the_first_number_is_raised_to_the_second_number.py
@@ -3,4 +3,5 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 #calculate the result of num1 raised to num2
+result = num1 ** num2
 #print the result
