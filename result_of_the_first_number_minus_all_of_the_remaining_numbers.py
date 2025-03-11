fb5cd7595f599fe11c7a15951c514f3abd826486
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-10 17:57:42 -0800

    print the result

--- result_of_the_first_number_minus_all_of_the_remaining_numbers.py
+++ result_of_the_first_number_minus_all_of_the_remaining_numbers.py
@@ -3,11 +3,13 @@ numbers = [0] * 10  #create a list with 10 elements initialized to 0
 
 #ask the user to input 10 numbers
 for i in range(10):
-        numbers[i] = float(input(f"Enter number {i + 1}: "))
+        numbers[i] = int(input(f"Enter number {i + 1}: "))
 
 #first number
 first_number = numbers[0]
-#compute the result of the first number minus all remaining numbers
-result = first_number
+
+result = first_number #compute the result of the first number minus all remaining numbers
 for num in numbers[1:]:
         result -= num
+
+print(result) #print the result
