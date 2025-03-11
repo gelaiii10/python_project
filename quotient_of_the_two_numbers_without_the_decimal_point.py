58b0e8821d32c79d93c0498281313e1941d4fe87
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-10 17:10:37 -0800

    added condition

--- quotient_of_the_two_numbers_without_the_decimal_point.py
+++ quotient_of_the_two_numbers_without_the_decimal_point.py
@@ -1,3 +1,6 @@
 #ask user to input two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+if num2 == 0: #test if the 2nd number is  not 0 to avoid error
+    print("ERROR: Division by zero is not allowed.") 
