6a2000f05222d0dcb395e85b1e0365859193b6a9
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-10 18:49:24 -0800

    use for loop and print the numbers between the two numbers

--- all_the_numbers_between_those_two_numbers.py
+++ all_the_numbers_between_those_two_numbers.py
@@ -4,3 +4,7 @@ num2 = int(input("Enter the second number: "))
 #determine the upper and lower bounds
 lower = min(num1, num2)
 upper = max(num1, num2)
+
+print(f"numbers between {lower} and {upper}:") #print all numbers between the two numbers
+for i in range(lower + 1, upper): 
+    print(i)
