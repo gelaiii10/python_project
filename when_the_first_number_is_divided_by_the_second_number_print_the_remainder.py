1cdebd359f87046f158093962971f2399e545642
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-10 17:32:05 -0800

    check if 2nd number is not zero

--- when_the_first_number_is_divided_by_the_second_number_print_the_remainder.py
+++ when_the_first_number_is_divided_by_the_second_number_print_the_remainder.py
@@ -1,3 +1,6 @@
 #ask user to input two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+if num2 == 0: #check if the second number is not zero to avoid division by zero
+    print("ERROR: Division by zero is not allowed.")
