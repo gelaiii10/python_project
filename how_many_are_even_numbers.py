3f7405e470830f9c52bba0e7a282fbca31955877
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-10 18:11:57 -0800

    compute the even numbers and print the result

--- how_many_are_even_numbers.py
+++ how_many_are_even_numbers.py
@@ -3,5 +3,7 @@ even_count = 0
 #ask user to input 10 numbers
 for i in range(10):
     num = int(input(f"Enter Number {i + 1}: "))
-#compute if the number is even
-#print the even numbers
+    if num % 2 == 0: #compute if the number is even
+        even_count += 1
+
+print(even_count) #print the even numbers
