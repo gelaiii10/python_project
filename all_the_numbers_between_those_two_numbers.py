f873f407704a09a44bdf0f0fe961df693c1119ca
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-10 18:46:16 -0800

    determine the upper and loower bounds

--- all_the_numbers_between_those_two_numbers.py
+++ all_the_numbers_between_those_two_numbers.py
@@ -1,3 +1,6 @@
 #ask user to input two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+#determine the upper and lower bounds
+lower = min(num1, num2)
+upper = max(num1, num2)
