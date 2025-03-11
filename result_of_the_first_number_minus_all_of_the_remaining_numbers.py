06655985d6e50aa68db9f8351d2b86095932f5a7
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-10 17:48:48 -0800

    added the first number

--- result_of_the_first_number_minus_all_of_the_remaining_numbers.py
+++ result_of_the_first_number_minus_all_of_the_remaining_numbers.py
@@ -4,3 +4,6 @@ numbers = [0] * 10  #create a list with 10 elements initialized to 0
 #ask the user to input 10 numbers
 for i in range(10):
         numbers[i] = float(input(f"Enter number {i + 1}: "))
+
+#first number
+first_number = numbers[0]
