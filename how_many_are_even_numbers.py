206e1e8cdf3ead8e615758117c342d950fd024ab
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-10 18:09:10 -0800

    added counter and used for loop to ask user to input numbers

--- how_many_are_even_numbers.py
+++ how_many_are_even_numbers.py
@@ -1,4 +1,7 @@
 #input a counter for even numbers
+even_count = 0
 #ask user to input 10 numbers
+for i in range(10):
+    num = int(input(f"Enter Number {i + 1}: "))
 #compute if the number is even
 #print the even numbers
