7bda04fd393999a2d3e99de5f6fdd03e0dbd7009
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-10 16:28:04 -0800

    determine the small number

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -2,4 +2,5 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 #determine the smaller number
+smaller_number = min(num1, num2)
 #print the smaller number
