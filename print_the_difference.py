3bf71ebf1ae8ad63d60e4888490ea80d459d5aca
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-10 16:59:35 -0800

    determine the difference

--- print_the_difference.py
+++ print_the_difference.py
@@ -1,3 +1,6 @@
 #ask user to input two numbers
 num1 = int(input("enter the first number: "))
 num2 = int(input("enter the second number: "))
+
+difference = num1 - num2 #compute the difference
+#print the difference
