ba16e878fd9e3d3e54cd53b50f3339cd9fb12948
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-11 21:59:17 -0800

    added condition to check if number is duplicate or unique

--- continuously_asks_the_user_to_input_numbers_until_an_invalid_input_is_provided.py
+++ continuously_asks_the_user_to_input_numbers_until_an_invalid_input_is_provided.py
@@ -4,5 +4,11 @@ print("input numbers (you need to type anything else to stop):")
 while True:
     user_input = input("Enter a number: ")
     
-    try: #try to convert input into float
-        number = float(user_input)  # convert input to a number
+    try: #try to convert input inti float
+        number = float(user_input)  #convert input to a number
+        #check if the number is already in the set
+        if number in seen_numbers:
+            print("duplicate")
+        else:
+            print("unique")
+            seen_numbers.add(number)  #add the number to the set
