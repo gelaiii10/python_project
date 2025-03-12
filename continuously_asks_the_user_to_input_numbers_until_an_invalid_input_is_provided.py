1f563f334c5a577fa7eeafb5e39745457a64b3dd
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-11 21:55:55 -0800

    try to convert input into float

--- continuously_asks_the_user_to_input_numbers_until_an_invalid_input_is_provided.py
+++ continuously_asks_the_user_to_input_numbers_until_an_invalid_input_is_provided.py
@@ -3,3 +3,6 @@ print("input numbers (you need to type anything else to stop):")
 
 while True:
     user_input = input("Enter a number: ")
+    
+    try: #try to convert input into float
+        number = float(user_input)  # convert input to a number
