77233033a5f64f4f81284b139a3d65c3fa37d1af
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-11 22:12:51 -0800

    convert input into float

--- program_will_then_display_the_lowest_number_that_was_entered.py
+++ program_will_then_display_the_lowest_number_that_was_entered.py
@@ -5,4 +5,6 @@ print("Enter numbers (you need to type anything else to stop):")
 while True:
     user_input = input("Enter a number: ")
 #try to convert the input to a float
+    try:
+        number = float(user_input)  # Convert input to a number
 #update the lowest number if it is the first valid input or if it is lower than the current lowest
