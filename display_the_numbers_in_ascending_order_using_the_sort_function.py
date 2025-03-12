f1d8c91b34bb5a143bb197b07a13ddd18f71f847
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-11 22:22:05 -0800

    add the numbers to the list

--- display_the_numbers_in_ascending_order_using_the_sort_function.py
+++ display_the_numbers_in_ascending_order_using_the_sort_function.py
@@ -1,1 +1,9 @@
 numbers = [] #add list to store the numbers
+
+while True:
+    user_input = input("Enter a number: ")
+
+    #try to convert the input to a float
+    try:
+        number = float(user_input)  
+        numbers.append(number)  #add the number to the list
