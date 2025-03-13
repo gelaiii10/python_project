ab2dbb2cd16f0904c3354271b997b4ca516531d3
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-13 11:33:30 -0800

    added while loop

--- calculate_and_displays_the_average_of_the_entered_numbers.py
+++ calculate_and_displays_the_average_of_the_entered_numbers.py
@@ -1,2 +1,8 @@
 numbers = [0] * 100  #initialize a list with a fixed size
 count = 0  #to keep track of the number of valid inputs
+
+while True:
+    user_input = input("Enter a number: ")
+        #check for exit condition
+    if user_input.lower() == 'exit':
+            break
