76c2f73b5896201c8b8f6c3651470d8aa84e6ef0
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-13 11:04:00 -0800

    input highest num as none and check for exit condition

---
+++ displays_the_highest_number_entered_by_the_user.py
@@ -0,0 +1,8 @@
+highest_number = None #input highest number as none
+
+while True:
+    user_input = input("Enter a number: ")
+        
+        #check for exit condition
+    if user_input.lower() == 'exit':
+        break
