fa78462c870df9cbcbc6282a2a1f7e088e54276a
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-13 11:20:09 -0800

    created an empty list and added while loop

---
+++ sorts_the_numbers_in_descending_order_and_displays_them_from_highest_to_lowest.py
@@ -0,0 +1,7 @@
+numbers = []  #input an empty list to store the numbers
+    
+while True:
+    user_input = input("Enter a number: ")
+        
+    if user_input.lower() == 'exit': #check for exit condition
+            break
