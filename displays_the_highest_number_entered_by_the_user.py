61f2f5be8d87cf4f33877454aca0a84d6d01625c
gelaiii10 <gelaibacleaan@gmail.com>
2025-03-13 11:08:20 -0800

    update highest number if its none or the new number is greater

--- displays_the_highest_number_entered_by_the_user.py
+++ displays_the_highest_number_entered_by_the_user.py
@@ -6,3 +6,11 @@ while True:
         #check for exit condition
     if user_input.lower() == 'exit':
         break
+
+    try:
+        num = float(user_input)
+            #update highest number if it's None or if the new number is greater
+        if highest_number is None or num > highest_number:
+                highest_number = num
+    except ValueError:
+            print("Invalid input. Please enter a valid number or type 'exit' to stop.")
